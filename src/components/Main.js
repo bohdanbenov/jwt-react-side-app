54500d1fc6b36c8d43a636d26ce34aa5099d0876
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-23 13:21:57 +0300

    Added Main component which show message if you successfully logged in.

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello, you are successfully logged in!</h1>
+            </div>
+        );
+    }
+}
+
+export default Main;
