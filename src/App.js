bcf8c584ec996ca794df25a57789dd21c2b815df
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-23 13:19:57 +0300

    Deleted all unused functionality.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
+import Header from "./components/Header";
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            email: '',
-            password: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        const target = event.target;
-        const value = target.type === 'email' ? target.value : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSubmit(event) {
-        alert('Your email: ' + this.state.email + ' Your password: ' + this.state.password);
-        event.preventDefault();
-    }
-
     render() {
         return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-          <div>
-              <form onSubmit={this.handleSubmit}>
-                  <label>
-                      Email:
-                      <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
-                  </label>
-                  <br/>
-                  <br/>
-                  <label>
-                      Password:
-                      <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                  </label>
-                  <input type="submit" value="Login" />
-              </form>
+          <div className="App">
+              <Header/>
+              <Login/>
           </div>
-      </div>
     );
   }
 }
